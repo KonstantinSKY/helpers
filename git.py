1c1c82d07c18abb287d5d53914797b72a959e529
sky <sky012877@gmail.com>
2021-03-10 10:06:38 -0800

    gf

--- git.py
+++ git.py
@@ -1,9 +1,12 @@
+# Helper for git, to use it quickly
+
 import os
 import sys
 import subprocess
 
+
 def bash(command):
-    print('\033[92m '+command+"\033[0m")
+    print('\033[92m ' + command + "\033[0m")
     os.system(command)
 
 
@@ -26,7 +29,7 @@ def add():
 
 def commit(msg=None):
     bash('git status')
-    print("Commiting and Pushing")
+    print("Committing and Pushing")
     if msg is None:
         msg = input("Enter commit message 'git commit -m' \n")
         if msg == "":
@@ -37,13 +40,13 @@ def commit(msg=None):
         bash(f'git commit -m "Deleting file: {msg}"')
 
 
-print("-"*100)
+print("-" * 100)
 print(" -= GIT helper =-")
 directory = os.path.abspath(os.curdir)
 print("Path for Git:", directory)
 print("-" * 100)
 
-p = str(subprocess.check_output("git status", shell=True)).lstrip("'b/'").rstrip("'").replace("\\n", "\n")\
+p = str(subprocess.check_output("git status", shell=True)).lstrip("'b/'").rstrip("'").replace("\\n", "\n") \
     .replace("\\t", "\t").replace("\\'", "\'")
 print(p)
 if p.find("working tree clean") >= 0:
@@ -57,10 +60,5 @@ if len(sys.argv) == 2 and sys.argv[1] == 'del':
 else:
     add()
 
-
 bash('git push')
 bash('git status')
-
-
-
-
