c5963d3faf7ae337be969e41cc293a8c764626bf
sky <sky012877@gmail.com>
2021-03-10 16:44:21 -0800

    step 2

--- git_clone.py
+++ git_clone.py
@@ -6,7 +6,7 @@ print(f'Working directory will be created right here:{os.getcwd()}')
 link = input('Enter the SSH link of repository and Push any key...\n')
 directory = link[link.find('/')+1:-4]
 # TODO check fo directory and rename
-print(f'Will be created new directory: {os.getcwd}/{directory}')
+print(f'Will be created new and working directory: {os.getcwd}/{directory}')
 # TODO add copy gitignore and create needed
 # os.system(f'git clone {link}')
 # print("Done")
