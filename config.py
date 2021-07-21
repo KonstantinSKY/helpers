8270f6dcb0922714ac0f66c5b979e49cd33e966c
sky <sky012877@gmail.com>
2021-07-21 13:34:21 -0700

    added config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import configparser
+print("test")
+
+NoPrint = {'AllLogs': False,
+           'InfoLogs': False,
+           'ErrLogs': False}
+
+
+if __name__ == "__main__":
+    print("test2")
+    print(NoPrint)
