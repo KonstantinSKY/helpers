f714a52eef0bbc8f2cc592c119fce62ba73b7815
sky <sky012877@gmail.com>
2021-07-22 23:45:52 -0700

    NO MESSAGE

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Conf(ConfigParser):
     def __init__(self, name):
         super().__init__()
         self.optionxform = str
-        self.path = ConfigDirPath+'/'+name
+        self.path = ConfigDirPath+'/'+os.path.basename(name).split('.')[0]+'.conf'
 
         if not os.path.exists(ConfigDirPath) or not os.path.isdir(ConfigDirPath):
             print('Config directory not found. It will be created automatically.')
@@ -25,14 +25,30 @@ class Conf(ConfigParser):
 
         self.read(self.path)
 
+    def save(self):
+        with open(self.path, "w+") as file:
+            self.write(file)
+
+    def chk_add_key_data(self, key: str, data: dict):
+        if key not in self:
+            self[key] = data
+            self.save()
+            return
+
+        for option in data:
+            if option not in self[key]:
+                self[key][option] = data[option]
+        self.save()
+
 
 if __name__ == "__main__":
     print("test2")
-    configg = Conf('testname')
+    configg = Conf('testname.py')
     print(os.getcwd())
     configg.read('config/testname')
     print(configg)
 
     print(configg['DEFAULT']['TestSettings'])
-    for key in configg:
-        print(key)
+    # for key in configg:
+    #     print(key)
+    configg.chk_add_key_data('LOGGER', {'dfsd': 'sdfsdf'})
--- logger.py
+++ logger.py
@@ -3,30 +3,43 @@
 import logging
 from config import Conf
 
+default_config = {
+    'AllLogs': True,
+    'InfoLogs': True,
+    'ErrorLogs': True,
+    'WarningLogs': True,
+    'Printing': True
+    }
+
+config = Conf(__file__)
+config.chk_add_key_data('ALL_MODULES_LOGS', default_config)
+
 Green = '\033[92m'
 Red = '\033[91m'
 Styles_end = '\033[0m'
 
-class Logger:
+
+class Logger():
     def __init__(self, name):
-        # self.err_logger = logging.getLogger(f'err_{name}')
-        # self.info_logger = logging.getLogger(f'info_{name}')
-        #
-        # self.err_logger.setLevel(logging.ERROR)
-        # self.info_logger.setLevel(logging.INFO)
-        #
-        # fh_err = logging.FileHandler(f"logs/err_{name}.log")
-        # fh_info = logging.FileHandler(f"logs/info_{name}.log")
-        #
-        # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        # fh_err.setFormatter(formatter)
-        # fh_info.setFormatter(formatter)
-        #
-        # self.err_logger.addHandler(fh_err)
-        # self.info_logger.addHandler(fh_info)
-        #
-        # self.allow_log = True
-        # self.allow_print = True
+
+        self.err_logger = logging.getLogger(f'err_{name}')
+        self.info_logger = logging.getLogger(f'info_{name}')
+
+        self.err_logger.setLevel(logging.ERROR)
+        self.info_logger.setLevel(logging.INFO)
+
+        fh_err = logging.FileHandler(f"logs/err_{name}.log")
+        fh_info = logging.FileHandler(f"logs/info_{name}.log")
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh_err.setFormatter(formatter)
+        fh_info.setFormatter(formatter)
+
+        self.err_logger.addHandler(fh_err)
+        self.info_logger.addHandler(fh_info)
+
+        self.allow_log = True
+        self.allow_print = True
 
     def prn_info(self, msg):
         if self.allow_print:
@@ -54,6 +67,6 @@ class Logger:
 
 
 if __name__ == "__main__":
-    # log = Logger(__name__)
-    # log.prn_log_info('Green')
-    # log.prn_log_err('Red')
+    log = Logger(__name__)
+    log.prn_log_info('Green')
+    log.prn_log_err('Red')
