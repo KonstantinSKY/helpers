e3c1dfccec8422df9ef2078b5c8e6226d2bd62ed
sky <sky012877@gmail.com>
2021-07-22 20:41:28 -0700

    cleaned config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 from configparser import ConfigParser
-import config
 import os
 
 ConfigDirPath = 'config'
-
-print("test")
 
 
 class Conf(ConfigParser):
@@ -25,48 +22,15 @@ class Conf(ConfigParser):
                 self.write(file)
 
         self.read(self.path)
-        # print(self.path)
-        # print(self.sections())
-
-
-    #
-# if not os.path.exists(ConfigDirPath) or not os.path.isdir(ConfigDirPath):
-#     print('Config directory not found')
-#     os.mkdir(ConfigDirPath)
-#
-# path = ConfigDirPath+'/'+logger
-#
-#
-#     config['NO_PRINT'] = {"AllLogs": 'ЫВАeq',
-#                       'IL': '234',
-#                       'ErrLogs': False}
-#     config['NO_PRINT']['DfdDdfd'] = 'FGH'
-#
-#     conf = {"AllLoDFgs": 'ЫВАdeq', 'ILdf': '234', 'ErrDDDDDLogs': False, 'DfdDdfd': 'FGH'}
-#
-#     print(conf)
-#
-# for key in config['NO_PRINT']:
-#     print(key)
-#
-# with open('logger.conf', 'w') as configfile:
-#     config.write(configfile)
 
 
 if __name__ == "__main__":
     print("test2")
-    # print(config['NO_PRINT'])
-    # print(config)
-    # print(config.sections())
-    # for key in config['NO_PRINT']:
-    #     print(key)
     configg = Conf('testname')
     print(os.getcwd())
     configg.read('config/testname')
     print(configg)
-    # config = ConfigParser()
-    # config.read('config/testname')
-    # print(config.sections())
+
     print(configg['DEFAULT']['TestSettings'])
     for key in configg:
         print(key)
