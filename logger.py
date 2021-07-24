e72d144763493eb2314158700833f666c26054de
sky <sky012877@gmail.com>
2021-07-24 00:02:15 -0700

    logger.py test2

--- logger.py
+++ logger.py
@@ -14,7 +14,7 @@ default_logs = {
     'WarningPrint': 'YES',
     }
 
-path = "../logs/"
+path = "logs/"
 
 glob_config = Conf(__file__)
 # if 'ALL_MODULES_LOGS' in glob_config:
@@ -24,6 +24,7 @@ glob_conf = glob_config['ALL_MODULES_LOGS']
 print(glob_conf['AllLogs'])
 
 Green = '\033[92m'
+Yellow = '\033[93m'
 Red = '\033[91m'
 Styles_end = '\033[0m'
 
@@ -46,20 +47,26 @@ class Logger:
         if glob_conf['InfoLogs'].upper() == "YES" and self.conf['InfoLogs'].upper() == 'YES':
             self.info_logger = logging.getLogger(f'info_{name}')
             self.info_logger.setLevel(logging.INFO)
+
             fh_info = logging.FileHandler(f"{path}info_{name}.log")
             fh_info.setFormatter(formatter)
+            self.info_logger.addHandler(fh_info)
 
         if glob_conf['ErrorLogs'].upper() == "YES" and self.conf['ErrorLogs'].upper() == 'YES':
             self.err_logger = logging.getLogger(f'err_{name}')
             self.err_logger.setLevel(logging.ERROR)
+
             fh_err = logging.FileHandler(f"{path}err_{name}.log")
             fh_err.setFormatter(formatter)
+            self.err_logger.addHandler(fh_err)
 
         if glob_conf['WarningLogs'].upper() == "YES" and self.conf['WarningLogs'].upper() == 'YES':
             self.warn_logger = logging.getLogger(f'warn_{name}')
             self.warn_logger.setLevel(logging.WARNING)
+
             fh_warn = logging.FileHandler(f"{path}warn_{name}.log")
             fh_warn.setFormatter(formatter)
+            self.warn_logger.addHandler(fh_warn)
 
     def print_log(self, message):
         if glob_conf['AllPrint'].upper() == 'NO' or self.conf['AllPrint'].upper() == 'NO':
@@ -70,6 +77,7 @@ class Logger:
         if glob_conf['InfoPrint'].upper() == "YES" and self.conf['InfoPrint'].upper() == 'YES':
             self.print_log(f'{Green}{msg}')
         if self.info_logger is not None:
+            print('try to save')
             self.info_logger.info(msg)
 
     def err(self, msg):
@@ -80,7 +88,7 @@ class Logger:
 
     def warn(self, msg):
         if glob_conf['WarningPrint'].upper() == "YES" and self.conf['WarningPrint'].upper() == 'YES':
-            self.print_log(f'{Green}{msg}')
+            self.print_log(f'{Yellow}{msg}')
         if self.warn_logger is not None:
             self.warn_logger.warning(msg)
 
